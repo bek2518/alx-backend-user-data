ec5e5c0b607249ceba59b517a6ee517aaba8247a
bek2518 <bereketb20@hotmail.com>
2023-05-24 16:48:27 +0300

    Removed the extra semi-colon

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,7 @@ def filter_datum(fields, redaction, message, separator):
     Function that takes arguments returns log message obfuscated
     '''
     for field in fields:
-        regex = '{}=.+?{}'.format(field, separator)
-        redaction = '{}={}{}'.format(field, redaction, separator)
+        regex = '{}=.+?(?={})'.format(field, re.escape(separator))
+        redaction = '{}={}'.format(field, redaction)
         message = re.sub(regex, redaction, message)
     return message
