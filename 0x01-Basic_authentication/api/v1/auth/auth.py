f9351193e8091a97f01ced70bbc3fe9264386b0c
bek2518 <bereketb20@hotmail.com>
2023-05-29 11:48:32 +0300

    Implemented validation for all requests

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -30,7 +30,10 @@ class Auth:
         '''
         Public method that returns None for now
         '''
-        return None
+        if (request is None) or ('Authorization' not in request.headers):
+            return None
+
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''
