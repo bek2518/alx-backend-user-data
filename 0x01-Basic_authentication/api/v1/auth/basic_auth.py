6c224bcc3334390e301cb6eb292a2202a44a7b61
bek2518 <bereketb20@hotmail.com>
2023-05-29 12:31:13 +0300

    Base64 decoding implemented

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Holds the BasicAuth class that inherits from Auth
 '''
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,19 @@ class BasicAuth(Auth):
                 (authorization_header[0:6] != 'Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        '''
+        Method that returns the decoded value of the base64 string argument
+        '''
+        if (base64_authorization_header is None) or\
+           (type(base64_authorization_header) != str):
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+        except Exception:
+            return None
+
+        return decoded.decode('utf-8')
