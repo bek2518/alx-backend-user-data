957e6cc3cef8d655f0099c7a5cdf7575140ba152
bek2518 <bereketb20@hotmail.com>
2023-05-29 11:54:42 +0300

    Implements empty BasicAuth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Holds the BasicAuth class that inherits from Auth
+'''
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    '''
+    Inherits from Auth and does nothing for now
+    '''
+    pass
