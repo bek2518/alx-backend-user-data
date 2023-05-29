9bac1a3d0ad0b60b6c0d86aa5a4f02ed894c5cf9
bek2518 <bereketb20@hotmail.com>
2023-05-29 12:23:47 +0300

    Pycodestyle edit

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,11 +9,13 @@ class BasicAuth(Auth):
     '''
     Inherits from Auth and does nothing for now
     '''
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         '''
         Method that returns the Base64 patr of the authorization header
         '''
-        if (authorization_header is None) or (type(authorization_header) != str) or\
-            (authorization_header[0:6] != 'Basic '):
+        if (authorization_header is None) or\
+            (type(authorization_header) != str) or\
+                (authorization_header[0:6] != 'Basic '):
             return None
         return authorization_header[6:]
