d9341607c806713c53c73ad1be41fc76e2c80872
bek2518 <bereketb20@hotmail.com>
2023-06-02 11:23:04 +0300

    Rearranged position

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -46,12 +46,13 @@ def before():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.current_user(request) is None:
-        abort(403)
-
     if (auth.authorization_header(request) is None) and\
        (auth.session_cookie(request) is None):
         abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
 
     request.current_user = auth.current_user(request)
 
