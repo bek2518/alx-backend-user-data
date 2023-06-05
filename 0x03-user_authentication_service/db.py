8f0fcbe1a6945b5a3d5b71111c33f1c4c1295c79
bek2518 <bereketb20@hotmail.com>
2023-06-05 15:16:06 +0300

    Return first row found based on kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 class DB:
@@ -35,4 +37,17 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs):
+        '''
+        Method that takes arbitrary keyword argument and returns the
+        first row foind in the users table
+        '''
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except Exception:
+            raise InvalidRequestError
+        if user is None:
+            raise NoResultFound
         return user
