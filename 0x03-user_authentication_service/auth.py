e5982682e0452c9a515a9249e939ba39be18def5
bek2518 <bereketb20@hotmail.com>
2023-06-06 10:25:04 +0300

    Register a user handling duplicates

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Auth module that handles the authorization process
 '''
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +15,24 @@ def _hash_password(password: str) -> bytes:
     password = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''
+        Method that hashes a password and save user to the database
+        '''
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
+
+        raise ValueError('User {} already exists'.format(email))
