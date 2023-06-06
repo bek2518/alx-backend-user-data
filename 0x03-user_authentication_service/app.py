e3cdc8e72b88c437f74da1003e828b58861bfa2e
bek2518 <bereketb20@hotmail.com>
2023-06-06 11:29:36 +0300

    Implement route to validate login and set cookie

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 '''
 Basic Flask app
 '''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -30,6 +30,21 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": email, "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    '''
+    Function that implements POST /sessions to validate login information
+    '''
+    email = request.form['email']
+    password = request.form['password']
+    if Auth.valid_login(email, password):
+        session_id = Auth.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    abort(401)
 
 
 if __name__ == "__main__":
