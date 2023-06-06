cba28413dad8366e24bd32b12e2ec4035d713343
bek2518 <bereketb20@hotmail.com>
2023-06-06 10:42:38 +0300

    Implement POST route to register

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 '''
 Basic Flask app
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+Auth = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -14,6 +16,20 @@ def index():
     Get route that returns jsonified message
     '''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    '''
+    Function that implements POST /user route
+    '''
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        Auth.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
