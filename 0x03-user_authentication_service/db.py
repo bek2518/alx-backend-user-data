6b4d0580f6094c38ff0a7abe400e68550768e2f1
bek2518 <bereketb20@hotmail.com>
2023-06-06 09:42:15 +0300

    Reverted back and removed echo

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,10 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-
-from user import Base, User
 
 
 class DB:
@@ -18,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -40,3 +39,30 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        '''
+        Method that takes arbitrary keyword argument and returns the
+        first row foind in the users table
+        '''
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except Exception:
+            raise InvalidRequestError
+        if user is None:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        '''
+        Finds user using find_use_by and updated the value passed as
+        kwargs
+        '''
+        user = self.find_user_by(id=user_id)
+        attributes = ['email', 'hashed_password', 'session_id', 'reset_token']
+        for key, value in kwargs.items():
+            if key not in attributes:
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+        return None
