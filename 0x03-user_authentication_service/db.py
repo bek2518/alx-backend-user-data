a9fc0e62d84c1cbac68807af8aecda003c73a2d5
bek2518 <bereketb20@hotmail.com>
2023-06-06 09:34:16 +0300

    pycode style edit and moved import of user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import Base, User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+
+from user import Base, User
 
 
 class DB:
@@ -38,5 +39,4 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
-    
         return user
