61028e65738940f44f6607c554823c84e701dbcf
bek2518 <bereketb20@hotmail.com>
2023-06-06 09:30:03 +0300

    Trial with changing comment

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,15 +32,11 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        '''
-        Method that saves the user to the database
-        '''
-        try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
-        except Exception:
-            return
+        """Method that saves the user to the database
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
